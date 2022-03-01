7022fdb2568aaba57c20f7404a3959fdeade3eaa
JayWh0 <90079476+JayWh0@users.noreply.github.com>
2022-03-01 07:01:43 -0500

    Create yes.lua

---
+++ yes.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/mlartix/Lore-Game/main/Lore-Game-Teleport-Script"))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
